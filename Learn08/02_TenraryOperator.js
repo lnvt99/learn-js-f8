3d5c5d750267c6fc0345c376d8ebf756c6ca518d
ThangLe <lnvthang.it@gmail.com>
2022-05-12 23:40:01 +0700

    Chaper 09: Done

---
+++ Learn08/02_TenraryOperator.js
@@ -0,0 +1,8 @@
+// Toán tử 3 ngôn - Ternary Operator
+var course = {
+    name: 'JavaScript',
+    coin: 250
+};
+
+var result = course.coin > 0 ? `${course.coin} Coin` : `Free`;
+console.log(result);
